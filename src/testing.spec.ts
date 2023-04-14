d781122cb6165788c075053e1cea3db5bc63b10d
Rafael Costa <rafael.costa.fw@gmail.com>
2023-04-14 09:22:02 -0300

    chore: remove test files

--- src/testing.spec.ts
+++
@@ -1,7 +0,0 @@
-import { Test } from './testing'
-
-it('should work', () => {
-  const test = new Test()
-
-  expect(test.execute()).toBe('Testing')
-})
--- src/testing.ts
+++
@@ -1,7 +0,0 @@
-export class Test {
-  public execute() {
-    const test = 'Just a test #3'
-    console.log(test)
-    return 'Testing'
-  }
-}
